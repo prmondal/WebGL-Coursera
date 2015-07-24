97015eda5d3619df0b4099a81f5c785cb79bb2cb
prasenjit <prasenjuetce@gmail.com>
2015-07-25 00:11:24 +0530

    Added brush, line, background properties and clear btn

--- assignment2/paint.js
+++ assignment2/paint.js
@@ -2,40 +2,55 @@
 var canvas;
 var gl;
 
+//curve properties
 var maxNumVertices = 100000;
 var curveIdx = 0;
 var numberOfVerticesCurve = [0];
 var curveStart = [0];
-
-var mouseUp = false, 
-    mouseDown = false, 
-    mouseMove = false;
-
 var curveStarted = false;
 
-var point, index = 0, lastIndex = 0;
+var point, index = 0;
 
-var paintColor = vec2(1.0, 1.0, 0.0, 1.0);
-var lineWidth = 2.0;
+var mouseDown = false;
 
-window.onload = function init() {
-    canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-    
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-    gl.clear( gl.COLOR_BUFFER_BIT );
+//brush
+var defaultBrushColor = '#00ff00';
+var brushColor;
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+var lineWidth = 1;
+
+//canvas
+var defaultCanvasBackgroundColor = '#000000';
+var canvasBackgroundColor;
+
+//gl
+var vBuffer, cBuffer, program;
+
+/*var canvasXPos = window.innerWidth,
+    canvasYPos = 0,
+    canvasWidth = 512,
+    canvasHeight = 512;*/
+
+//convert from HEX color value to RGB
+function hexToRGB(hex) {
+    hex = hex.substr(1);
     
+    var rgb = parseInt(hex, 16);
+
+    var r = (rgb >> 16) & 0xFF;
+    var g = (rgb >> 8) & 0xFF;
+    var b = rgb & 0xFF;
     
-    var vBuffer = gl.createBuffer();
+    return vec4(r / 255.0, g / 255.0, b / 255.0, 1.0);
+}
+
+function clearCanvas() {
+    //delete old buffers
+    gl.deleteBuffer(vBuffer);
+    gl.deleteBuffer(cBuffer);
+
+    //initialize buffers
+    vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, 8 * maxNumVertices, gl.STATIC_DRAW);
     
@@ -43,26 +58,72 @@ window.onload = function init() {
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
     
-    var cBuffer = gl.createBuffer();
+    cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW );
     
     var vColor = gl.getAttribLocation( program, "vColor");
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-    
+
+    //reset
+    index = 0;
+    curveIdx = 0;
+    numberOfVerticesCurve = [0];
+    curveStart = [0];
+}
+
+function initDOM() {
+    //initialize color palette
+    $("#brush-color").colorpicker({
+        //showOn: "button",
+        color: defaultBrushColor,
+        defaultPalette: 'web'
+    });
+
+    //update paint color
+    $("#brush-color").on("change.color", function(e, color){
+        brushColor = hexToRGB(color);
+    });
+
+    $("#background-color").colorpicker({
+        //showOn: "button",
+        color: defaultCanvasBackgroundColor,
+        defaultPalette: 'web'
+    });
+
+    //update paint color
+    $("#background-color").on("change.color", function(e, color){
+        canvasBackgroundColor = hexToRGB(color);
+
+        gl.clearColor( canvasBackgroundColor[0], canvasBackgroundColor[1], canvasBackgroundColor[2], canvasBackgroundColor[3]);
+    });
+
+    //slider listener
+    /*$("#slider-line-width").change(function(e) {
+        lineWidth = $(this).val();
+    });*/
+
+    $('#select-line-width').change(function(){
+        lineWidth = $(this).val();
+    });
+
+    $("#clear-canvas").click(function(e) {
+        clearCanvas();
+    });
+
+    canvas = document.getElementById( "gl-canvas" );
+}
+
+function initCanvas() {
+    clearCanvas();
+
     canvas.addEventListener("mousedown", function(e) {
         mouseDown = true;
-        mouseUp = false;
-        mouseMove = false;
-
-        //lastIndex = index;
     });
 
     canvas.addEventListener("mouseup", function(e) {
         mouseDown = false;
-        mouseUp = true;
-        mouseMove = false;
 
         //curve ended
         if(curveStarted) {
@@ -75,12 +136,9 @@ window.onload = function init() {
     });
 
     canvas.addEventListener("mousemove", function(event){
-        mouseMove = true;
-        
         //start drawing
         if(mouseDown) {
             //console.debug('Mouse State: Pressed and Moving.');
-
             curveStarted = true;
 
             //insert vertices
@@ -91,7 +149,7 @@ window.onload = function init() {
 
             //add color to vertices
             gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-            gl.bufferSubData(gl.ARRAY_BUFFER, 16 * index, flatten(paintColor));
+            gl.bufferSubData(gl.ARRAY_BUFFER, 16 * index, flatten(brushColor));
 
             numberOfVerticesCurve[curveIdx]++;
 
@@ -104,9 +162,47 @@ window.onload = function init() {
     render();
 }
 
+function initPainConfig() {
+    //store paint default color
+    brushColor = hexToRGB(defaultBrushColor);
+
+    //store background default color
+    canvasBackgroundColor = hexToRGB(defaultCanvasBackgroundColor);
+}
+
+function initGL() {
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+
+
+    gl.clearColor( canvasBackgroundColor[0], canvasBackgroundColor[1], canvasBackgroundColor[2], canvasBackgroundColor[3]);
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+}
+
+window.onload = function init() {
+    initPainConfig();
+
+    initDOM();
+    
+    /*canvas.style.position = 'absolute';
+    canvas.style.top = (window.innerHeight / 2 - canvasHeight / 2) + 'px';
+    canvas.style.left = (window.innerWidth / 2 - canvasWidth / 2) + 'px';*/
+
+    initGL();
+
+    initCanvas();
+}
+
 
 function render() {
-    
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     //set line width
