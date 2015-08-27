9be2d6d8ad960020314c68dce839e1035df934aa
prasenjit <prasenjuetce@gmail.com>
2015-08-27 19:12:02 +0530

    Enhanced UI.

--- assignment4/main.js
+++ assignment4/main.js
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Prasenjit M
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 "use strict";
 
 var WORLD = {
@@ -23,7 +47,7 @@ var OBJECT_TYPE = {
 
 var SPHERE_PROPERTY = {
 	LONG_SUB_DIVISIONS: 64, //should be double of lat divs
-	LAT_SUB_DIVISIONS: this.LONG_SUB_DIVISIONS / 2,
+	LAT_SUB_DIVISIONS: 32,
 
 	thetaStart: 0,
 	thetaEnd: Math.PI,
